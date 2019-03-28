f5b31078f5a419b8fc1aa894b31b26a4cfb2feb1
Andrii <radomely@gmail.com>
2019-03-28 22:35:15 +0200

    fix CSS

--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -7,16 +7,33 @@ import {signInWithEmailAndPassword} from '../server';
 import {inputData} from '../redux/actions/LoginInputActions';
 
 const LoginForm = ({inputData, inputs}) => {
+    const inputValidationRegEx = {
+        email: /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim,
+        password:  /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/
+    }
+    let isEmailValid = inputValidationRegEx.email.test(inputs.email);
+    let isPasswordValid = inputValidationRegEx.password.test(inputs.password);
+
     return (
         <form onSubmit={(e) =>{ e.preventDefault(); signInWithEmailAndPassword(inputs.email, inputs.password)}} className="form flex-column center-box shadow max-width-500 padding-all-25" method="post">
             <h2 className="h2 text-center">Sign In</h2>
             <div className="form-group flex-end">
-                <label htmlFor="email">Email</label>
-                <input onChange={(e) => inputData(e)} value={inputs.email} className="form-input" type="email" name="email" placeholder="Enter your email" />
+                <label htmlFor="email" className={isEmailValid? "input__valid":"input__invalid"}>Email</label>
+                <input onChange={(e) => inputData(e)} 
+                        value={inputs.email} 
+                        className={inputs.email.length>0? "form-input " + (isEmailValid? "input__valid":"input__invalid"):"form-input"}
+                        type="email" 
+                        name="email" 
+                        placeholder="Enter your email" />
             </div>
             <div className="form-group flex-end">
-                <label htmlFor="password">Password</label>
-                <input onChange={(e) => inputData(e)} value={inputs.password} className="form-input" type="password" name="password" placeholder="Enter your password"/>
+                <label htmlFor="password" className={isEmailValid? "input__valid":"input__invalid"}>Password</label>
+                <input onChange={(e) => inputData(e)} 
+                        value={inputs.password} 
+                        className={inputs.password.length>0? "form-input " + (isPasswordValid? "input__valid":"input__invalid"):"form-input"}
+                        type="password" 
+                        name="password" 
+                        placeholder="Enter your password"/>
             </div>
             <a href="#" className="text-center form-group">Forgot Password?</a>
             <button className="btn" type="submit" label="Sign In">Sign In</button>
