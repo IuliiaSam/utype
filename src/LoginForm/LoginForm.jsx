c5f498abc6299f2bb4182f63237a8a39a49ff7a6
iRoy <44869601+monolithphoenix@users.noreply.github.com>
2019-03-29 17:41:10 +0200

    Fix V, del double css

--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-import './LoginForm.css';
+import '../Registration/Registration.css';
 
 import { signInWithEmailAndPassword } from '../server';
 
@@ -12,34 +12,35 @@ import {logedIn} from '../redux/actions/logedInAction';
 
 const LoginForm = ({ inputData, inputs, logedIn }) => {
   return (
-      <div className="LoginForm">
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-        signInWithEmailAndPassword(inputs.email, inputs.password).then(uid => logedIn(uid));
-        // logedIn();
+    <div className="Form-wrapper">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          signInWithEmailAndPassword(inputs.email, inputs.password).then(uid => logedIn(uid));
+          // logedIn();
 
-      }}
-      className="form flex-column center-box shadow max-width-500 padding-all-25"
-      method="post"
-    >
-      <h2 className="h2 text-center">Sign In</h2>
-        <input
-          onChange={e => inputData(e)}
-          value={inputs.email}
-          className="form-input"
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-        />
-        <input
-          onChange={e => inputData(e)}
-          value={inputs.password}
-          className="form-input"
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />
+        }}
+        className="Form shadow"
+        method="post"
+      >
+      <h2 className="Form__title">Sign In</h2>
+      <input
+        onChange={e => inputData(e)}
+        value={inputs.email}
+        className="Form__input"
+        type="email"
+        name="email"
+        placeholder="Enter your email"
+        autoFocus
+      />
+      <input
+        onChange={e => inputData(e)}
+        value={inputs.password}
+        className="Form__input"
+        type="password"
+        name="password"
+        placeholder="Enter your password"
+      />
       <button className="btn" type="submit" label="Sign In">
         Sign In
       </button>
--- src/Registration/Registration.jsx
+++ src/Registration/Registration.jsx
@@ -24,84 +24,68 @@ const Registration = ({readInputValue, inputs}) => {
     let isAllInputsValueCorrect = emailOK && nameOK && passOK && confpassOK;      
 
     return (
-        <form 
-            className="Registration-form flex-column center-box shadow max-width-500 padding-all-25 margin-top-50"
-            onSubmit={e => {
-                e.preventDefault();
-                createUserWithEmailAndPassword(
-                    inputs.email,
-                    inputs.password,
-                    inputs.name
-                    );
-                }}
+        <div className="Form-wrapper">
+            <form 
+                className="Form shadow"
+                onSubmit={e => {
+                    e.preventDefault();
+                    createUserWithEmailAndPassword(
+                        inputs.email,
+                        inputs.password,
+                        inputs.name
+                        );
+                    }}
             >
-
-            <h1 className="text-center">Sign Up</h1>
-
-            <div className={inputs.email.length === 0 ? "Registration-form__group" : emailOK ? "Registration-form__group success" : "Registration-form__group error"}>
-                <label htmlFor="email"className="input-label"><span>Email</span></label>
-                <div className="input-conteiner">
+                <h2 className="Form__title">Sign Up</h1>
+                <label className={inputs.email.length === 0 ? "Form-label" : emailOK ? "Form-label success" : "Form-label error"}>
                     <input 
                         type="email" 
                         name="email" 
-                        className="Registration-form__input" 
+                        className="Form__input" 
                         placeholder="Email" 
                         onChange={readInputValue}
+                        autoFocus
                     />
                     {inputs.email.length === 0 ? null : emailOK ? null : <small>Invalid email address.</small>}
-                </div>
-            </div>
-
-            <div className={inputs.name.length === 0 ? "Registration-form__group" : nameOK ? "Registration-form__group success" : "Registration-form__group error"}>
-                <label htmlFor="name"className="input-label"><span>Username</span></label>
-                <div className="input-conteiner">
+                </label>
+                <label className={inputs.name.length === 0 ? "Form-label" : nameOK ? "Form-label success" : "Form-label error"}>
                     <input 
                         type="text" 
                         name="name" 
-                        className="Registration-form__input" 
+                        className="Form__input" 
                         placeholder="Username" 
                         onChange={readInputValue}
                     />
-                    {inputs.name.length === 0 ? null : nameOK ? null : <small>Username must be at least 4 characters long and contain only letters and numbers [a-z, A-Z, 0-9].</small>}
-                </div>
-            </div>
-
-            <div className={inputs.password.length === 0 ? "Registration-form__group" : passOK ? "Registration-form__group success" : "Registration-form__group error"}>
-                <label htmlFor="password"className="input-label"><span>Password</span></label>
-                <div className="input-conteiner">
+                    {inputs.name.length === 0 ? null : nameOK ? null : <small>Username must be at least 4 characters long and contain only letters and numbers.</small>}
+                </label>
+                <label className={inputs.password.length === 0 ? "Form-label" : passOK ? "Form-label success" : "Form-label error"}>
                     <input 
                         type="password" 
                         name="password" 
-                        className="Registration-form__input" 
+                        className="Form__input" 
                         placeholder="Password" 
                         aria-autocomplete="list" 
                         onChange={readInputValue}
                     />
-                    {inputs.password.length === 0 ? null : passOK ? null : <small>Password must be at least 4 characters long and contain only letters and numbers [a-z, A-Z, 0-9].</small>}
-                </div>
-            </div>
-                 
-            <div className={!passOK || inputs.confirm_password.length === 0 ? "Registration-form__group" : confpassOK ? "Registration-form__group success" : "Registration-form__group error"}>
-                <label htmlFor="confirm_password"className="input-label"><span>Confirm password</span></label>
-                <div className="input-conteiner">
+                    {inputs.password.length === 0 ? null : passOK ? null : <small>Password must be at least 4 characters long and contain only letters and numbers.</small>}
+                </label>
+                <label className={!passOK || inputs.confirm_password.length === 0 ? "Registration-form__group" : confpassOK ? "Registration-form__group success" : "Registration-form__group error"}>
                     <input 
                         type="password" 
                         name="confirm_password" 
-                        className="Registration-form__input" 
+                        className="Form__input" 
                         placeholder="Confirm password" 
                         onChange={readInputValue}
                     />
                     {!passOK || inputs.confirm_password.length === 0 ? null : confpassOK ? null : <small>Password does not match the confirm password.</small>}
-                </div>
-            </div>
-
-            <Button disabled={!isAllInputsValueCorrect}/>
-
-            <small className="text-center">
-                <span>Already have an account? </span>
-                <NavLink className="link" to="/login">Sign In</NavLink>
-            </small>
-        </form>
+                </label>
+                <Button disabled={!isAllInputsValueCorrect}/>
+                <small className="text-center">
+                    <span>Already have an account? </span>
+                    <NavLink className="link" to="/login">Sign In</NavLink>
+                </small>
+            </form>
+        </div>
     );
 };
 
